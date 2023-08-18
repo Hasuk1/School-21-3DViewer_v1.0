f999fc9a82a83bac87ad2546909395bb15da1a32
Hasuk1 <92762015+Hasuk1@users.noreply.github.com>
2023-08-18 03:32:29 +0300

    Added piece of UI

--- src/view/3DViewer/mainwindow.cpp
+++ src/view/3DViewer/mainwindow.cpp
@@ -28,13 +28,27 @@ void MainWindow::on_renderObjFile_clicked()
     QString fileNameStr = ui->pathToObjFile->text();
     QByteArray fileNameUtf8 = fileNameStr.toUtf8();
     char *fileName = fileNameUtf8.data();
-    if (fileName[0] !='\0') {
+    if (QFile::exists(fileName)) {
     ui->OGLWindow->parse_file(fileName);
+    QString vCount = QString::number(ui->OGLWindow->data.vertices_v_count);
+    unsigned int ffCount = ui->OGLWindow->data.polygonals_f_count/2;
+    QString fCount = QString::number(ffCount);
+    ui->veticesCount->setText(vCount);
+    ui->edgesCount->setText(fCount);
+        char *start = strrchr(fileName, '/') + 1;
+        char *ext = strrchr(fileName, '.');
+        *ext = '\0';
+        ui->objName->setText(start);
+    } else {
+        QMessageBox::information(this, "ERROR", "Enter a path to file correctly");
+        ui->veticesCount->setText("");
+        ui->edgesCount->setText("");
+        ui->objName->setText("");
     }
+
 }
 
 void MainWindow::on_closeObject_clicked()
 {
     ui->OGLWindow->closeObject();
 }
-
